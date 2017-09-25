5814e716ebbef528149b2af87c1c9339cfc3195b
Gabriel Garcia <me@garciat.com>
2017-09-25 11:45:07 -0300

    Create valid-parenthesis-string.cpp

---
+++ leetcode/valid-parenthesis-string.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/valid-parenthesis-string/
+
+class Solution {
+public:
+    bool checkValidString(string s) {
+        return work(s, 0);
+    }
+    
+    bool work(const string& s, int i, int p = 0) {
+        if (p < 0) return false;
+        if (i >= s.size()) return p == 0;
+        switch (s[i]) {
+            case '(':
+                return work(s, i+1, p+1);
+            case ')':
+                return work(s, i+1, p-1);
+            case '*':
+                return work(s, i+1, p) || work(s, i+1, p+1) || work(s, i+1, p-1);
+        }
+    }
+
