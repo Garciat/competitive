3d35b25ed0b198ebc93b65dab4b87edd8c3e4184
Gabriel Garcia <me@garciat.com>
2017-10-08 12:35:31 -0300

    Update stickers-to-spell-word.cpp

--- leetcode/stickers-to-spell-word.cpp
+++ leetcode/stickers-to-spell-word.cpp
@@ -1,89 +1,102 @@
 // https://leetcode.com/problems/stickers-to-spell-word/
 
-using letter_count = array<int, 27>;
+struct ordered_letter_set {
+    
+    void insert(char c) {
+        counts[c - 'a'] += 1;
+    }
+    
+    bool empty() const {
+        for (int x : counts) {
+            if (x > 0) return false;
+        }
+        return true;
+    }
+    
+    bool contains(char c) const {
+        return counts[c - 'a'];
+    }
+    
+    bool has_empty_intersect(const ordered_letter_set &rhs) const {
+        for (int i = 0; i < counts.size(); ++i) {
+            if (counts[i] && rhs.counts[i])
+                return false;
+        }
+        return true;
+    }
+    
+    friend ordered_letter_set operator-(const ordered_letter_set& c, const ordered_letter_set& d) {
+        ordered_letter_set ans{};
+        for (int i = 0; i < ans.counts.size(); ++i) {
+            ans.counts[i] = max(0, c.counts[i] - d.counts[i]);
+        }
+        return ans;
+    }
+    
+    friend bool operator==(const ordered_letter_set& c, const ordered_letter_set& d) {
+        return c.counts == d.counts;
+    }
+    
+    array<int, 26> counts;
+};
+
 
 namespace std {
     template <>
-    struct hash<letter_count>
+    struct hash<ordered_letter_set>
     {
-        std::size_t operator()(const letter_count& k) const
+        std::size_t operator()(const ordered_letter_set& k) const
         {
             size_t h = 0;
-
-            for (int x : k) {
+            for (int x : k.counts)
                 h = (h + (324723947 + x)) ^ 93485734985;
-            }
-
             return h;
         }
     };
 }
 
-letter_count operator-(const letter_count& c, const letter_count& d) {
-    letter_count ans{};
-    for (int i = 0; i < ans.size(); ++i) {
-        ans[i] = max(0, c[i] - d[i]);
-    }
-    return ans;
-}
-
-bool is_done(const letter_count &c) {
-    for (int x : c) {
-        if (x > 0) return false;
-    }
-    return true;
-}
-
-bool contributes(const letter_count &tgt, const letter_count &c) {
-    for (int i = 0; i < tgt.size(); ++i) {
-        if (tgt[i] && c[i]) return true;
-    }
-    return false;
-}
-
-void print(letter_count c) {
-    for (int x : c) cout << x << ' ';
-    cout << endl;
-}
 
 class Solution {
 public:
     int minStickers(vector<string>& stickers, string target) {
         
-        letter_count total_count{};
+        ordered_letter_set all_letters{};
         
-        vector<letter_count> counts(stickers.size(), letter_count{});
+        vector<ordered_letter_set> word_letters(stickers.size(), ordered_letter_set{});
         
         for (int i = 0; i < stickers.size(); ++i) {
             for (char c : stickers[i]) {
-                counts[i][c-'a'] += 1;
-                total_count[c-'a'] += 1;
+                word_letters[i].insert(c);
+                all_letters.insert(c);
             }
         }
         
-        letter_count needed{};
+        ordered_letter_set needed{};
         
         for (char c : target) {
-            needed[c-'a'] += 1;
-            if (total_count[c-'a'] == 0) {
+            needed.insert(c);
+            if (!all_letters.contains(c)) {
                 return -1;
             }
         }
         
-        return solve(counts, needed);
+        return solve(word_letters, needed);
     }
     
-    unordered_map<letter_count, int> memo;
+    unordered_map<ordered_letter_set, int> memo;
     
-    int solve(vector<letter_count> &counts, letter_count target) {
-        if (is_done(target)) return 0;
-        int& ans = memo[target];
+    int solve(vector<ordered_letter_set> &word_letters, ordered_letter_set needed) {
+        if (needed.empty()) return 0;
+        
+        int& ans = memo[needed];
         if (ans != 0) return ans;
+        
         int tmp = 1000;
-        for (auto&& c : counts) {
-            if (contributes(target, c))
-                tmp = min(tmp, solve(counts, target - c));
+        for (auto&& candidate : word_letters) {
+            if (!needed.has_empty_intersect(candidate))
+                tmp = min(tmp, solve(word_letters, needed - candidate));
         }
+        
         return ans = 1 + tmp;
     }
 };
