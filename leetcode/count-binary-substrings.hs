a33fa906367d19e6106d1210257442dfbb580f42
Gabriel Garcia <me@garciat.com>
2018-01-12 17:38:53 -0300

    Create count-binary-substrings.hs

---
+++ leetcode/count-binary-substrings.hs
@@ -0,0 +1,14 @@
+splitByEqualRuns :: Eq a => [a] -> [[a]]
+splitByEqualRuns [] = []
+splitByEqualRuns (x:xs) =
+    let (as, bs) = span (== x) xs
+    in (x:as) : splitByEqualRuns bs
+
+
+pairwiseWith :: (a -> a -> b) -> [a] -> [b]
+pairwiseWith f xs = zipWith f xs (tail xs)
+
+
+countBinarySubstrings :: Eq a => [a] -> Int
+countBinarySubstrings =
+    sum . pairwiseWith min . map length . splitByEqualRuns
